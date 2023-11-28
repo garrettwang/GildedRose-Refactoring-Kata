eb0a94143c2c31c62d611056c1a3807de666e5da
garrettwang <habseligkeit@gmail.com>
2023-11-28 22:23:20 +0100

    refactored by myself 28.11.23

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -66,7 +66,7 @@ class GildedRose {
 
         item.sellIn = item.sellIn - 1;
 
-        if (item.sellIn < 0 && (item.quality > 0)) {
+        if (item.sellIn < 0 && item.quality > 0) {
                 item.quality = item.quality - 1;
 
         }
