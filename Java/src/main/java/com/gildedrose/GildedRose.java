3cce75140fce72918d1c68f0f3cac957356d5235
garrettwang <habseligkeit@gmail.com>
2023-11-28 22:21:36 +0100

    refactored by myself 28.11.23

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -18,10 +18,9 @@ class GildedRose {
             agedBrie(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             backstage(item);
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            sulfuras(item);
-        } else {
+        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             others(item);
+
         }
 
     }
@@ -41,14 +40,7 @@ class GildedRose {
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
-        }
-    }
-
-    private static void sulfuras(Item item) {
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-            }
+            item.quality = 0;
         }
     }
 
@@ -60,10 +52,9 @@ class GildedRose {
 
         item.sellIn = item.sellIn - 1;
 
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
+        if (item.sellIn < 0 && (item.quality < 50)) {
                 item.quality = item.quality + 1;
-            }
+
         }
     }
 
@@ -75,10 +66,9 @@ class GildedRose {
 
         item.sellIn = item.sellIn - 1;
 
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
+        if (item.sellIn < 0 && (item.quality > 0)) {
                 item.quality = item.quality - 1;
-            }
+
         }
     }
 }
