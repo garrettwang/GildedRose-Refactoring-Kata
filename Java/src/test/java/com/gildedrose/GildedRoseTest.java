8a33e83d23a09304437d8edc1bb112567da12df3
garrettwang <habseligkeit@gmail.com>
2023-11-30 20:38:10 +0100

    add a test before refactoring

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,15 @@
 package com.gildedrose;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +20,47 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void gildedRoseAfter20Days() throws URISyntaxException, IOException {
+        URL url = GildedRoseTest.class.getResource("/gilded-rose-21-days.txt");
+        URI uri = Objects.requireNonNull(url).toURI();
+        Path path = Path.of(uri);
+        byte[] bytes = Files.readAllBytes(path);
+        String expected = new String(bytes);
+        String actual = gildedRose(21);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    private String gildedRose(int days) {
+        StringBuilder result = new StringBuilder();
+        result.append("OMGHAI!").append("\n");
+
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < days; i++) {
+            result.append("-------- day ").append(i).append(" --------").append("\n");
+            result.append("name, sellIn, quality").append("\n");
+            for (Item item : items) {
+                result.append(item).append("\n");
+            }
+            result.append("\n");
+            app.updateQuality();
+        }
+        return result.toString();
+    }
 }
