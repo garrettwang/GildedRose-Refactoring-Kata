dc6a0ab1c4cb7989fdb11820446666bf40e183a8
garrettwang <habseligkeit@gmail.com>
2023-12-02 20:30:58 +0100

    refactored

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,8 +3,6 @@ package com.gildedrose;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class GildedRoseTest {
     @Test
     void updateQuality() {
