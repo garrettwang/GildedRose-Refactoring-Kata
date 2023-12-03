668cce7243eedef267991b624bb0c966dccf5476
garrettwang <habseligkeit@gmail.com>
2023-12-03 16:17:33 +0100

    added a test with approval tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +9,41 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void updateQuality() {
+        Item[] initItems = initialize();
+        GildedRose testee = new GildedRose(initItems);
+        StringBuilder result = new StringBuilder();
+
+        for (int day = 0; day < 21; day++) {
+         testee.updateQuality();
+            result.append("-------- day ").append(day).append(" --------").append("\n");
+            result.append("name, sellIn, quality").append("\n");
+            for (Item item: testee.items ) {
+                result.append(item.toString()).append("\n");
+            }
+        }
+        Approvals.verify(result.toString());
+
+    }
+
+    private Item[] initialize() {
+        return new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6)};
+    }
 }
