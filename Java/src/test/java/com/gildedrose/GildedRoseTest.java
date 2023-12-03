8ba569dd5479fb5c9786cf38bf0bc75fb78beb5d
garrettwang <habseligkeit@gmail.com>
2023-12-03 19:41:31 +0100

    refactored GildedRose with polymorphism.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void updateQuality() {
+        GildedRose testee = new GildedRose(initialize());
+        StringBuilder result = new StringBuilder();
+        for (int day = 0; day < 21; day++) {
+            testee.updateQuality();
+            result.append("-------- day " + day + " --------").append("\n");
+            result.append("name, sellIn, quality").append("\n");
+            for (Item item : testee.items) {
+                result.append(item.toString()).append("\n");
+            }
+        }
+        Approvals.verify(result.toString());
     }
 
+    private Item[] initialize() {
+        return new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6) };
+    }
 }
