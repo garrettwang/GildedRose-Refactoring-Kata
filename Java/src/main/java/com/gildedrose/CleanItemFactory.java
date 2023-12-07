8ba1c925f9c19429e9e752dd6e00920b93b3fde9
garrettwang <habseligkeit@gmail.com>
2023-12-07 20:47:03 +0100

    refactored

--- Java/src/main/java/com/gildedrose/CleanItemFactory.java
+++ Java/src/main/java/com/gildedrose/CleanItemFactory.java
@@ -6,22 +6,12 @@ public class CleanItemFactory {
     }
 
     static CleanItem createCleanItem(String name, int sellIn, int quality) {
-        switch (name) {
-            case "Aged Brie" -> {
-                return new AgedBrie(sellIn, quality);
-            }
-            case "Backstage passes to a TAFKAL80ETC concert" -> {
-                return new Backstage(sellIn, quality);
-            }
-            case "Sulfuras, Hand of Ragnaros" -> {
-                return new Sulfuras(sellIn, quality);
-            }
-            case "Conjured Mana Cake" -> {
-                return new Conjured(sellIn, quality);
-            }
-            default -> {
-                return new CleanItem(name, sellIn, quality);
-            }
-        }
+        return switch (name) {
+            case "Aged Brie" -> new AgedBrie(sellIn, quality);
+            case "Backstage passes to a TAFKAL80ETC concert" -> new Backstage(sellIn, quality);
+            case "Sulfuras, Hand of Ragnaros" -> new Sulfuras(sellIn, quality);
+            case "Conjured Mana Cake" -> new Conjured(sellIn, quality);
+            default -> new CleanItem(name, sellIn, quality);
+        };
     }
 }
